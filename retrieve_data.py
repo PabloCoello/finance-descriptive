2d4e0cf83e54e2e577a8341db58fd4d4abb518e0
Pablo Coello <pablo.coello@outlook.es>
2020-03-22 23:52:13 +0100

    Simplified everything with yfinance API

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,27 +1,4 @@
-import sys
-import pandas as pd 
-from yahoo_finance_api2 import share
-from yahoo_finance_api2.exceptions import YahooFinanceError
-from datetime import datetime, date
-
-def request_data(code, period, frequency):
-    my_share = share.Share(code)
-    symbol_data = None
-
-    try:
-        symbol_data = my_share.get_historical(share.PERIOD_TYPE_MONTH,
-                                            period,
-                                            share.FREQUENCY_TYPE_DAY,
-                                            frequency)
-    except YahooFinanceError as e:
-        print(e.message)
-        sys.exit(1)
-    return(pd.DataFrame(symbol_data))
-
-def format_timestamp(df):
-    df['timestamp'] = (df['timestamp']/1000).apply(datetime.fromtimestamp, inplace=True)
-    df['timestamp'] = df['timestamp'].apply(date.isoformat)
-    return(df)
-
-df = request_data('^IBEX', 12, 1)
-df = format_timestamp(df)
+import yfinance as yf
+period = ['2019-01-01', '2019-12-31']
+data = yf.download("^IBEX", start=period[0], end=period[1])
+data['Open'].plot()
